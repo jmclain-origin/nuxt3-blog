b4af85bd3b9664bed18c86426698d8276d469fed
Josh McLain <joshmclain45@gmail.com>
2023-05-11 09:10:24 -0700

    ‼️ BREAKING:  incomplete auth and session

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,23 +1,27 @@
 <script setup>
-import { onMounted } from "vue";
-import { Modal, initModals } from "flowbite";
-const { status, data, signIn, signOut} = useAuth();
+import { onMounted, ref } from "vue";
+import { Modal } from "flowbite";
+
+const { status, data, signIn, signOut } = useAuth();
+const auth = ref({ status: '', user: data.user });
+
+
 onMounted(() => {
+  console.log(data?.user)
   const $buttonElement = document.querySelector("#button");
   const $modalElement = document.querySelector("#authentication-modal");
   const $closeButton = document.querySelector("#closeButton");
+  console.log("AUTH", status.value, status.effect);
+    console.log("DATA", data.value);
   const modalOptions = {
     backdropClasses:
       "bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40",
   };
-  if ($modalElement) {
+  if ($modalElement && $buttonElement) {
     const modal = new Modal($modalElement, modalOptions);
     $buttonElement.addEventListener("click", () => modal.toggle());
     $closeButton.addEventListener("click", () => modal.hide());
-    console.log('AUTH', status);
-    console.log('DATA', data);
-    // programatically show
-    // modal.show();
+    
   }
 });
 </script>
@@ -25,20 +29,15 @@ onMounted(() => {
 <template>
   <!-- Modal toggle -->
   <button
+    v-if="auth.status === 'unauthenticated'"
     data-modal-target="authentication-modal"
     data-modal-toggle="authentication-modal"
-    class="block text-white text-base md:text-xl bg-primary-600 hover:bg-secondary-800 focus:ring-4 focus:outline-none focus:ring-secondary-300 font-medium rounded-lg px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    class="text-white text-base md:text-xl bg-primary-600 hover:bg-secondary-500 hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-secondary-300 font-medium rounded-full px-5 py-2.5 text-center block shadow-lg hover:shadow-accent-500/60 transition-all ease-in-out duration-200 active:translate-y-2"
     type="button"
     id="button"
   >
-    Sign up
-  </button>
-  <button
-    class="block text-white text-base md:text-xl bg-primary-600 hover:bg-secondary-800 focus:ring-4 focus:outline-none focus:ring-secondary-300 font-medium rounded-lg px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    type="button"
-    @click="signOut"
-  >
-    Sign out
+    <span class="leading-5"> check us out </span>
+    <Icon name="ic:outline-arrow-forward-ios" class="ml-3 -translate-y-[0.30px]" size="16" />
   </button>
 
   <!-- Main modal -->
@@ -52,41 +51,54 @@ onMounted(() => {
       <!-- Modal content -->
       <div class="relative bg-zinc-400 rounded-lg shadow">
         <button
-        id="closeButton"
-        type="button"
-        class="absolute top-3 right-2.5 text-gray-900 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-        data-modal-hide="authentication-modal"
+          id="closeButton"
+          type="button"
+          class="absolute top-3 right-2.5 text-gray-900 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+          data-modal-hide="authentication-modal"
         >
-        <svg
-        aria-hidden="true"
-        class="w-5 h-5"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-        >
-        <path
-        fill-rule="evenodd"
-        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-        clip-rule="evenodd"
-        ></path>
-      </svg>
-      <span class="sr-only">Close modal</span>
-    </button>
-    <div class="px-6 py-6 lg:px-8">
-      <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-        Sign in with an authorized client
-      </h3>
-      <form class="space-y-5" action="#">
-
-        <button type="button" @click="signIn('github')" class="social-btn bg-[#181616]">GitHub <Icon class="ml-3" name="bi:github"/></button>
-        <button class="social-btn bg-[#4a154b]" disabled>Slack <Icon class="ml-3" name="bi:slack"/></button>
-        <button class="social-btn bg-[#1ed760]" disabled>Spotify <Icon class="ml-3" name="bi:spotify"/></button>
-        <button class="social-btn bg-[#db4437]" disabled>Google <Icon class="ml-3" name="bi:google"/></button>
-        <button class="social-btn bg-[#1877f2]" disabled>Facebook <Icon class="ml-3" name="bi:facebook"/></button>
-        
-            
-            
-            
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="sr-only">Close modal</span>
+        </button>
+        <div class="px-6 py-6 lg:px-8">
+          <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+            Sign in with an authorized client
+          </h3>
+          <form class="space-y-5" action="#">
+            <button
+              type="button"
+              @click="signIn('github')"
+              class="social-btn bg-[#181616]"
+            >
+              GitHub <Icon class="ml-3" name="bi:github" />
+            </button>
+            <button class="social-btn bg-[#4a154b]" disabled>
+              Slack <Icon class="ml-3" name="bi:slack" />
+            </button>
+            <button class="social-btn bg-[#1ed760]" disabled>
+              Spotify <Icon class="ml-3" name="bi:spotify" />
+            </button>
+            <button
+              type="button"
+              @click="signIn('google')"
+              class="social-btn bg-[#db4437]"
+            >
+              Google <Icon class="ml-3" name="bi:google" />
+            </button>
+            <button class="social-btn bg-[#1877f2]" disabled>
+              Facebook <Icon class="ml-3" name="bi:facebook" />
+            </button>
           </form>
         </div>
       </div>
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,5 +1,7 @@
 import { NuxtAuthHandler } from '#auth'
 import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google';
 export default NuxtAuthHandler({
     secret: process.env.AUTH_SECRET,
     providers: [
@@ -7,6 +9,25 @@ export default NuxtAuthHandler({
         GithubProvider.default({
            clientId: process.env.GITHUB_CLIENT_ID,
            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+        // @ts-expect-error You need to use.default here for it to work during SSR. May be fixed via Vite at some point
+        GoogleProvider.default({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            scope: ['profile', 'email'],
+
+        }),
+        // @ts-expect-error You need to use.default here for it to work during SSR. May be fixed via Vite at some point
+        CredentialsProvider.default({
+            name: 'Credentials',
+            credentials: {
+                username: { label: 'Username', type: 'text', placeholder: 'Username' },
+                password: { label: 'Password', type: 'text', placeholder: 'Password' },
+            },
+            authorize(credentials: any) {
+                return { credentials }
+            }
+
         })
     ]
 })
